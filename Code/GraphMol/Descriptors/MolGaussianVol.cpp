1265f7f096d2634e700558dc058b582bf27f1368
jbraun <jessica.braun@phys.chem.ethz.ch>
2025-09-12 10:07:13 +0200

    adding updates for reformatting

--- Code/GraphMol/Descriptors/MolGaussianVol.cpp
+++ Code/GraphMol/Descriptors/MolGaussianVol.cpp
@@ -17,7 +17,26 @@ namespace Descriptors {
 namespace {
 
 double getD2CutOff(){
-  return std::numeric_limits<double>::max();;
+  return std::numeric_limits<double>::max();
+}
+
+double getA_ak( double alpha1, double alpha2 ){
+  double a_ak = static_cast< double >( 0 );
+  double sum = alpha1 + alpha2;
+  if( sum > static_cast<double>( 0 ) ){
+    a_ak = alpha1*alpha2 / sum;
+  }
+  return a_ak;
+}
+
+double getOverlap0( double alpha1, double alpha2 ){
+  double a_pi = static_cast< double>( 0 );
+  double sum = alpha1 + alpha2;
+  if( sum > static_cast<double>( 0 ) ){
+    a_pi = pi / sum;
+  }
+  double a_oe = _p2_ * a_pi * std::sqrt( a_pi );
+  return a_oe;
 }
 
 void ComputeGaussianVolume(
@@ -49,7 +68,7 @@ void ComputeGaussianVolume(
                 double a_oe = getOverlap0( alpha1, alpha2 );
                 double expMinus_ = exp( - d2 * a_ak );
                 o += a_oe * expMinus_;
-        }
+            }
     }
 } // end of anonymous namespace
 } // end of Descriptors namespace
