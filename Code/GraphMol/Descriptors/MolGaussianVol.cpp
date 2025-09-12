ea802561d87de2d5972724f5e24a6002fbbdd838
jbraun <jessica.braun@phys.chem.ethz.ch>
2025-09-12 11:48:21 +0200

    fixes

--- Code/GraphMol/Descriptors/MolGaussianVol.cpp
+++ Code/GraphMol/Descriptors/MolGaussianVol.cpp
@@ -14,6 +14,42 @@
 
 namespace RDKit {
 namespace Descriptors {
+    void ComputeGaussianVolume(
+                            const ROMol &mol, 
+                            int confId,
+                            double &o
+						 )
+    {
+        o = 0.0;
+        const RDKit::Conformer &conf = mol.getConformer(confId);
+        //get all atom positions
+        std::vector<RDGeom::Point3D> all_positions;
+        std::vector<double> alpha_vector;
+        for (unsigned int i = 0; i < mol.getNumAtoms(); ++i) {
+            RDGeom::Point3D pos = conf.getAtomPos(i);
+            all_positions.push_back(pos);
+            int atomic_num = mol.getAtomWithIdx(i)->getAtomicNum();
+            alpha_vector.push_back(getAlpha(vdw_radii.at(atomic_num)));
+        }
+        // loop over all atoms
+        for (unsigned int i = 0; i < mol.getNumAtoms(); ++i) {
+            RDGeom::Point3D pos1 = all_positions[i];
+            for (unsigned int j = 0; j < i; ++j) {
+                RDGeom::Point3D pos2 = all_positions[j];
+                double d2 = (pos1.x - pos2.x)*(pos1.x - pos2.x) + 
+                            (pos1.y - pos2.y)*(pos1.y - pos2.y) + 
+                            (pos1.z - pos2.z)*(pos1.z - pos2.z);
+                if ( d2 < getD2CutOff() ) {
+                    double alpha1 = alpha_vector[i];
+                    double alpha2 = alpha_vector[j];
+                    double a_ak = getA_ak( alpha1, alpha2 );
+                    double a_oe = getOverlap0( alpha1, alpha2 );
+                    double expMinus_ = exp( - d2 * a_ak );
+                    o += a_oe * expMinus_;
+                }
+            }
+        }
+    }
 namespace {
 
 const std::map<unsigned int, double> vdw_radii = {
@@ -94,41 +130,7 @@ double getAlpha( double r ){
   return alpha;
 }
 
-void ComputeGaussianVolume(
-                            const ROMol &mol, 
-                            int confId,
-                            double &o
-						 )
-{
-    o = 0.0;
-    const RDKit::Conformer &conf = mol.getConformer(confId);
-    //get all atom positions
-    std::vector<RDGeom::Point3D> all_positions;
-    std::vector<double> alpha_vector;
-    for (unsigned int i = 0; i < mol.getNumAtoms(); ++i) {
-        RDGeom::Point3D pos = conf.getAtomPos(i);
-        all_positions.push_back(pos);
-        int atomic_num = mol.getAtomWithIdx(i)->getAtomicNum();
-        alpha_vector.push_back(getAlpha(vdw_radii.at(atomic_num)));
-    }
-    // loop over all atoms
-    for (unsigned int i = 0; i < mol.getNumAtoms(); ++i) {
-        RDGeom::Point3D pos1 = all_positions[i];
-        for (unsigned int j = 0; j < i; ++j) {
-            RDGeom::Point3D pos2 = all_positions[j];
-            double d2 = (pos1.x - pos2.x)*(pos1.x - pos2.x) + 
-                        (pos1.y - pos2.y)*(pos1.y - pos2.y) + 
-                        (pos1.z - pos2.z)*(pos1.z - pos2.z);
-            if ( d2 < getD2CutOff() ) {
-                double alpha1 = alpha_vector[i];
-                double alpha2 = alpha_vector[j];
-                double a_ak = getA_ak( alpha1, alpha2 );
-                double a_oe = getOverlap0( alpha1, alpha2 );
-                double expMinus_ = exp( - d2 * a_ak );
-                o += a_oe * expMinus_;
-            }
-        }
-    }
+
 } // end of anonymous namespace
 } // end of Descriptors namespace
 } // end of RDKit namespace
