8203ab934434a7f56af1ad72246bdd9632249364
cschiebroek <carl.schiebroek@phys.chem.ethz.ch>
2025-09-12 10:58:20 +0200

    change wrapper for MGV

--- Code/GraphMol/Descriptors/Wrap/rdMolDescriptors.cpp
+++ Code/GraphMol/Descriptors/Wrap/rdMolDescriptors.cpp
@@ -1176,7 +1176,7 @@ BOOST_PYTHON_MODULE(rdMolDescriptors) {
               (python::arg("mol")), docString.c_str());
   python::scope().attr("_CalcNumLipinskiHBD_version") =
       RDKit::Descriptors::lipinskiHBDVersion;
-  python::def("MolGaussianVol", RDKit::Descriptors::MolGaussianVol,
+  python::def("MolGaussianVol", RDKit::Descriptors::ComputeGaussianVolume,
           (python::args("self", "mol"), python::args("confId") = -1));
   python::scope().attr("_MolGaussianVol_version") = "1.0.0";
   docString = "returns the number of Lipinski H-bond acceptors for a molecule";
