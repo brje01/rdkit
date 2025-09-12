59c1c9c09d9a42a829d308a497de598f42835af4
jbraun <jessica.braun@phys.chem.ethz.ch>
2025-09-12 11:06:44 +0200

    fixing the header file

--- Code/GraphMol/Descriptors/MolGaussianVol.h
+++ Code/GraphMol/Descriptors/MolGaussianVol.h
@@ -1,6 +1,16 @@
+namespace RDKit {
+class ROMol;
+namespace Descriptors {  
+  
   // from the original shape paper and the pubchem implementation of it
   const double pi = 3.14159265358979323846;
   const double _p_ = 2.7;
   const double _p2_ = _p_ * _p_;
   const double lambda = ( 4.0 * pi ) / ( 3.0 * _p_ );     
   const double kappa = pi / pow( lambda, ( 2.0 / 3.0 ) ); 
+
+  RDKIT_DESCRIPTORS_EXPORT void ComputeGaussianVolume(const ROMol &mol, 
+                                                     int confId,
+                                                     double &o);
+}
+}
